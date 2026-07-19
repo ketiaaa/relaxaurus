02046343401c17d090849f39f886ad2f291f665d
ketia <ketia2007@gmail.com>
2026-07-19 21:57:27 +1000

    Fix cookie settings: secure+sameSite none for Cloudflare proxy

--- web/server.js
+++ web/server.js
@@ -170,8 +170,8 @@ function setUserCookie(res, user) {
   const token = jwt.sign(user, JWT_SECRET, { expiresIn: '12h' });
   res.cookie('token', token, {
     httpOnly: true,
-    secure: false,
-    sameSite: 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 12 * 60 * 60 * 1000,
     path: '/',
   });
