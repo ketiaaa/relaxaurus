ecccb11672920f9ed0bc13570572b071fa972a41
ketia <ketia2007@gmail.com>
2026-07-20 01:10:56 +1000

    Add Cache-Control headers to prevent Cloudflare caching old JS

--- web/server.js
+++ web/server.js
@@ -36,7 +36,14 @@ function audit(user, action, detail = '') {
 app.use(helmet({ contentSecurityPolicy: { directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"], styleSrc: ["'self'", "'unsafe-inline'"], imgSrc: ["'self'", "https://cdn.discordapp.com"], connectSrc: ["'self'"] } } }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'), { index: false, maxAge: 0, etag: false, lastModified: false }));
+// Prevent Cloudflare + browser caching of static assets
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  next();
+});
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
 app.use(morgan('short'));
 
 const apiLimiter = rateLimit({ windowMs: 60 * 1000, max: 30 });
